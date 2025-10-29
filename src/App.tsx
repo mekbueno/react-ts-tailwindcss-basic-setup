2c94a777a8b431143d823c3331a631676fdb651d
michael bueno <mek.bueno@gmail.com>
2025-10-29 12:59:12 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div className="bg-green-600 flex justify-center">
+				<h1 className="font-semibold text-2xl">Welcome to React with Vite</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
